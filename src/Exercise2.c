7048ebe1c2dfddfe2b95032c5da29aeeccd93850
TaKhang <ITITIU20226@student.hcmiu.edu.vn>
2021-10-04 23:54:06 +0700

    Update Ex1

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -13,19 +13,33 @@ Ex:
 #include <stdlib.h>
 #include <math.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	//testing variable, applying it to your algorithm for auto-evaluating
 	argc--;
-	int testcase[argc],i;
-	for(i=0; i<argc;i++){
-		if(atoi(argv[i+1])==0){
+	int testcase[argc], i;
+	for (i = 0; i < argc; i++)
+	{
+		if (atoi(argv[i + 1]) == 0)
+		{
 			argc = i;
 			break;
 		}
 		else
-			testcase[i] = atoi(argv[i+1]);
+			testcase[i] = atoi(argv[i + 1]);
 	}
-	//Your codes here
-
+	
+	int max, min;
+	max = testcase[0];
+	min = testcase[0];
+	for (i = 1; i < argc; i++)
+	{
+		if (testcase[i] >= max)
+			max = testcase[i];
+		else if (testcase[i] < min)
+			min = testcase[i];
+	}
+	printf("\nMax: %d", max);
+	printf("\nMin: %d ", min);
 	return 0;
 }
--- src/Exercise3.c
+++ src/Exercise3.c
@@ -15,7 +15,27 @@ Ex:
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int testcase = atoi(argv[1]);
+	int i,j;
+		for(i=1;i<=argc;i++)
+		{
+			for(j=2;j<=i-1;j++)	
+			if(i%j == 0)
+			{
+				break;
+			}
+		}
+			 if (i==j);
+				{
+				  printf("%d",i);
+				}
+			
 	//Your codes here
+	// Ở câu này em cứ hiểu cái testcase chính là thằng em nhập vô á rồi em xử lý bình thường thôi
+	// Quên để n như em 
+	// Thì em có thể để int n = testcase rồi xử lý cũng được á em hiểu he,
+	// dạ em sẽ cố 
+	// h thị em ko phiền chị nx 
+	// cảm ơn sư tỉ nha :v Ráng làm nè lát mà làm mắc đâu hỏi chị sau. bye bye
 	
 	return 0;
 }
